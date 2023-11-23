6140fa0c3f8a0ea11893dc53a2441a16dea364b1
dzikrimrputra <18221144@std.stei.itb.ac.id>
2023-11-23 13:21:11 +0700

    atur connection

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,18 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '0.5m', target: 100 },  // Ramp-up to 100 users over 2 minutes
-        { duration: '0.5m', target: 100 },  // Stay at 100 users for 5 minutes
-        { duration: '0.5m', target: 0 },
+        { duration: '0.1m', target: 100 },  // Ramp-up to 100 users over 2 minutes
+        { duration: '0.1m', target: 100 },  // Stay at 100 users for 5 minutes
+        { duration: '0.1m', target: 0 },
     ],
     thresholds: {
         http_req_duration: ['p(95)<500'], // Tambahkan threshold sesuai kebutuhan
     },
-    outputs: ['influxdb=http://localhost:8086/k6'],
+    // outputs: ['influxdb=http://localhost:8086/k6'],
 };
 
 export default function () {
-    let res = http.get('http://localhost:5000/movies');  // Ganti URL dengan URL aplikasi Anda
+    let res = http.get('http://app:5000/movies');  // Ganti URL dengan URL aplikasi Anda
     check(res, { 'status is 200': (r) => r.status === 200 });
     sleep(1);
 }
