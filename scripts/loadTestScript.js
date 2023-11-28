37888aa74e3b59fc14b200d2d25164931e445ec2
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-11-28 17:28:44 +0700

    stress test dan smoke test

--- scripts/loadTestScript.js
+++ scripts/loadTestScript.js
@@ -4,10 +4,8 @@ const mongoose = require("mongoose");
 
 export let options = {
   stages: [
-    { duration: "1m", target: 50 },
+    { duration: "2m", target: 50 },
     { duration: "5m", target: 50 },
-    { duration: "2m", target: 100 },
-    { duration: "5m", target: 100 },
     { duration: "2m", target: 0 },
   ],
 };
